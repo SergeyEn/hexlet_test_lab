3822738e6775c3d408427da671977ef1192fc8b7
SergeyEn <sergeyen.1995@gmail.com>
2023-05-16 12:35:09 +0300

    fix capitalize.py

--- test_lab/capitalize.py
+++ test_lab/capitalize.py
@@ -3,6 +3,6 @@
 def capitalize(text):
     if text == '':
         return ''
-    first_char = text[1].upper()
+    first_char = text[0].upper()
     rest_substring = text[1:]
     return f'{first_char}{rest_substring}'
